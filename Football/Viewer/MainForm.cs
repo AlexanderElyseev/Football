d31c2a51898d8175cb0016778b90313a3c0ec488
Alexander Elyseev <alexander.elyseev@gmail.com>
2011-10-05 21:34:47 +0600

    Initialization of repository.

---
+++ Football/Viewer/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Viewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
