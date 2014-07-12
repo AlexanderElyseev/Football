ccfeffc3077afb53405c55c73b82a9684fecd316
Alexander Elyseev <alexander.elyseev@gmail.com>
2014-07-12 16:09:00 +0600

    Added class for game field.

---
+++ Football.Core/Field.cs
@@ -0,0 +1,40 @@
+﻿namespace Football.Core
+{
+    using System;
+    using System.Diagnostics.Contracts;
+
+    public sealed class Field
+    {
+        private readonly float _width;
+
+        private readonly float _height;
+
+        private readonly float _lineWidth;
+
+        float Width
+        {
+            get { return _width; }
+        }
+
+        float Height
+        {
+            get { return _height; }
+        }
+
+        float LineWidth
+        {
+            get { return _lineWidth; }
+        }
+
+        public Field(float width, float height, float lineWidth)
+        {
+            Contract.Requires<ArgumentException>(width > 0);
+            Contract.Requires<ArgumentException>(height > 0);
+            Contract.Requires<ArgumentException>(lineWidth > 0);
+
+            _width = width;
+            _height = height;
+            _lineWidth = lineWidth;
+        }
+    }
+}
---
+++ Football.Core/FieldSizes.cs
@@ -0,0 +1,17 @@
+﻿namespace Football.Core
+{
+    static class FieldSizes
+    {
+        public const float MinWidth = 90f;
+        public const float MaxWidth = 120f;
+        public const float MinInternationalWidth = 100f;
+        public const float MaxInternationalWidth = 110f;
+        public const float RecommendedWidth = 105;
+
+        public const float MaxHeight = 45f;
+        public const float MinHeight = 90f;
+        public const float MinInternationalHeight = 64f;
+        public const float MaxInternationalHeight = 75f;
+        public const float RecommendedHeight = 68;
+    }
+}
